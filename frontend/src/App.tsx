38589b07e373b80dfd4cfc960f173fd0b6f4a5d2
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-10 14:59:51 +0500

    connect change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,22 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { sendMsg, connect} from '../api/index'
 import Header from '../components/header/header'
+import ChatHistory from '../components/ChatHistory/ChatHistory'
 
 function App() {
+  const [chatHistory, setchatHistory] = useState([])
+
+  useEffect(() => {
+     connect((msg) => {
+      console.log("New Message")
+      this.setState(prevState => ({
+        chatHistory: [...this.state.chatHistory, msg]
+      }))
+      console.log(this.state);
+    });
+  
+  }, [])
+  
 
   function send() {
     console.log("hello");
@@ -14,6 +28,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <ChatHistory chatHistory={this.state.chatHistory} />
       <button onClick={send}>Hit</button>
     </div>
   )
